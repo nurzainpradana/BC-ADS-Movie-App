55e34b15c3a9dae2b50ee26940960bf33b677d14
nurzainpradana <pradananurzain@gmail.com>
2020-11-02 23:36:31 +0700

    create SplashScreenAct

---
+++ app/src/main/java/com/zainpradana/bcads4/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.zainpradana.bcads4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
