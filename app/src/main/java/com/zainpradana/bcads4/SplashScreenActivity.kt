924fc770f4e6360f75c9aef2349f1a4cc3e6f013
nurzainpradana <pradananurzain@gmail.com>
2020-11-03 16:45:33 +0700

    resolve lottie

--- app/src/main/java/com/zainpradana/bcads4/SplashScreenActivity.kt
+++ app/src/main/java/com/zainpradana/bcads4/SplashScreenActivity.kt
@@ -1,13 +1,21 @@
 package com.zainpradana.bcads4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.zainpradana.bcads4.module.home.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+        }, 5000)
     }
 }
