e79c0c59b0b5e5761e0e49dbd52e2234bfd3338f
nurzainpradana <pradananurzain@gmail.com>
2020-11-04 16:54:28 +0700

    add helper for movie db

--- app/src/main/java/com/zainpradana/bcads4/module/details/DetailActivity.kt
+++ app/src/main/java/com/zainpradana/bcads4/module/details/DetailActivity.kt
@@ -1,33 +1,68 @@
 package com.zainpradana.bcads4.module.details
 
+import android.content.ContentValues
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.MediaController
 import com.zainpradana.bcads4.R
+import com.zainpradana.bcads4.database.DatabaseContract
+import com.zainpradana.bcads4.database.DatabaseContract.NoteColums.Companion.DESC
+import com.zainpradana.bcads4.database.DatabaseContract.NoteColums.Companion.GENRE
+import com.zainpradana.bcads4.database.DatabaseContract.NoteColums.Companion.POSTER
+import com.zainpradana.bcads4.database.DatabaseContract.NoteColums.Companion.RATING
+import com.zainpradana.bcads4.database.DatabaseContract.NoteColums.Companion.TITLE
+import com.zainpradana.bcads4.database.DatabaseContract.NoteColums.Companion.TRAILER
+import com.zainpradana.bcads4.database.DatabaseContract.NoteColums.Companion._ID
+import com.zainpradana.bcads4.database.MovieHelper
 import com.zainpradana.bcads4.model.FilmModel
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+    lateinit var data: FilmModel
+    lateinit var noteHelper: MovieHelper
+    private var values = ContentValues()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val data = intent.getParcelableExtra<FilmModel>("data")
+        data = intent.getParcelableExtra<FilmModel>("data")!!
 
+        noteHelper = MovieHelper.getInstance(this)
+        noteHelper.open()
+
+        initView()
+        initListener()
+
+    }
+
+    private fun initListener() {
+        val mediaConstroller = MediaController(this)
+        mediaConstroller.setAnchorView(videoView)
+        videoView.setMediaController(mediaConstroller)
+
+        videoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + data.trailer))
+        videoView.start()
+
+        iv_favorite.setOnClickListener {
+            values.put(_ID, data.id)
+            values.put(TITLE, data.judul)
+            values.put(DESC, data.desc)
+            values.put(GENRE, data.genre)
+            values.put(POSTER, data.poster)
+            values.put(TRAILER, data.trailer)
+            values.put(RATING, data.rating)
+
+            noteHelper.insert(values)
+        }
+    }
+
+    private fun initView() {
         if (data != null) {
             tv_title_detail.text = data.judul
             tv_genre.text = data.genre
             tv_desc.text = data.desc
-
-            val mediaConstroller = MediaController(this)
-            mediaConstroller.setAnchorView(videoView)
-            videoView.setMediaController(mediaConstroller)
-
-            videoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + data.trailer))
-            videoView.start()
         }
-
-
     }
 }
